62411ebca85df7b52ae2ed5e9d7c2805f78a5606
luca400 <alexluca.predoi2018@gmail.com>
2020-07-09 10:48:12 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+      <input />
+      <button>Add Todo</button>
+
+      <ul>
+        <li>Walk Dog</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
